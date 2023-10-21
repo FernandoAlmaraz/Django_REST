ce0d411bb19443672a459934a284eee4b5c2f2f3
FernandoAlmaraz <fernandoalmarazdelaquintana@gmail.com>
2023-10-20 21:48:24 -0400

    adding select by id without restframework

--- inmuebles/inmuebleslist_app/views.py
+++ inmuebles/inmuebleslist_app/views.py
@@ -8,3 +8,15 @@ def inmueble_list(request):
     inmuebles = Inmueble.objects.all()
     data = {"inmuebles": list(inmuebles.values())}
     return JsonResponse(data)
+
+
+def inmueble_detalle(request, pk):
+    inmueble = Inmueble.objects.get(pk=pk)
+    data = {
+        "direccion": inmueble.direccion,
+        "pais": inmueble.pais,
+        "image": inmueble.image,
+        "descripcion": inmueble.descripcion,
+        "active": inmueble.active,
+    }
+    return JsonResponse(data=data)
