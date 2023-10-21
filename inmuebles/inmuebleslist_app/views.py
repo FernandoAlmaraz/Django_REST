73cb3e6a38d715d15b0b48d2b4921666fa67c72b
FernandoAlmaraz <fernandoalmarazdelaquintana@gmail.com>
2023-10-20 21:31:07 -0400

    adding endpoint for consult data

--- inmuebles/inmuebleslist_app/views.py
+++ inmuebles/inmuebleslist_app/views.py
@@ -1,3 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from inmuebleslist_app.models import Inmueble
+
 
 # Create your views here.
+def inmueble_list(request):
+    inmuebles = Inmueble.objects.all()
+    data = {"inmuebles": list(inmuebles.values())}
+    return JsonResponse(data)
